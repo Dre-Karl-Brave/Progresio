efed0d92b64d5e4752fdbf4e0f3eb03953057b71
JPAVictoria <jpavictoria829@gmail.com>
2026-01-07 00:02:58 +0800

    feat: more setups

--- app/page.js
+++ app/page.js
@@ -2,3 +2,16 @@ export default function Home() {
   return <h1>Andrepogi</h1> // Don't use this as entry point,
   //  just create a button leading to progresio/login (entry point for actual system)
 }
+
+{
+  /** Karl do this:
+  
+  no need to use next auth, just custom jwt like the usual
+  Background: #FFFFFF (pure white)
+Surface / Cards: #F8FAFC (very light gray)
+Border / Divider: #E5E7EB
+Primary Text: #0F172A (almost-black navy)
+Secondary Text: #475569
+
+  */
+}
